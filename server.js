d49662d975b1ab3547f7ecf020fac3f2b7be9f02
Nickolas Kostyuchek <35410318+nickkosty@users.noreply.github.com>
2025-07-29 15:21:29 -0400

    Fix: load Firebase config from env vars

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const app = express();
 
 app.use(bodyParser.json());
 
-const serviceAccount = require('./firebase-key.json'); // You'll add this in a second
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  token_uri: "https://oauth2.googleapis.com/token",
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
